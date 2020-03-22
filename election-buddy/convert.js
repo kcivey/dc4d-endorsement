58dd7d09a8bc318f6500d44f856c8cd9749916cb
Keith C. Ivey <keith@iveys.org>
2020-03-22 13:21:48 -0400

    improve handling of ElectionBuddy results

--- election-buddy/convert.js
+++ election-buddy/convert.js
@@ -30,32 +30,38 @@ async function main() {
         }
         else {
             const csvData = await csvParse(chunk, {columns: true});
-            const convertedData = [];
-            for (const row of csvData) {
-                const candidates = Object.keys(row).length - 4;
-                const newRow = {Voter: row.Voter};
-                for (const [name, value] of Object.entries(row)) {
-                    if (/No Endorsement|Write-in|Voter|Ballot|Abstain/.test(name)) {
-                        newRow[name] = value;
-                        continue;
-                    }
-                    const m = name.match(/(\S+?)(?:\s*\([^)]*\))?$/i);
-                    assert(m, `Unexpected column header format "${name}"`);
-                    const newName = m[1];
-                    let newValue = /^\d+$/.test(value) ? (ranked ? candidates - value + 1 : +value) : value;
-                    if (newValue > 2) { // allow ranking only 2
-                        newValue = '';
-                    }
-                    newRow[newName] = newValue;
-                }
-                convertedData.push(newRow);
-            }
             assert(office, `Missing office for\n${chunk}`);
             assert(!votes[office], `Votes already seen for "${office}`);
+            const convertedData = [];
+            for (const row of csvData) {
+                convertedData.push(transformRow(row, ranked));
+            }
             votes[office] = convertedData;
             ranked = false;
             office = '';
         }
     }
     console.log(votes)
+}
+
+function transformRow(row, ranked) {
+    const candidates = Object.keys(row).length - 4;
+    const newRow = {Voter: row.Voter};
+    for (const [name, value] of Object.entries(row)) {
+        if (/Write-in|Voter|Ballot|Abstain/i.test(name)) {
+            if (name !== 'Ballot') {
+                newRow[name] = ranked ? value.replace(/(?<=\()(\d+)(?=\))/, (m, m1) => candidates - m1 + 1) : value;
+            }
+            continue;
+        }
+        const m = name.match(/(No Endorsement)|(\S+?)(?:\s*\([^)]*\))?$/i);
+        assert(m, `Unexpected column header format "${name}"`);
+        const newName = m[1] || m[2];
+        let newValue = /^\d+$/.test(value) ? (ranked ? candidates - value + 1 : +value) : value;
+        if (newValue > 2) { // allow ranking only 2
+            newValue = '';
+        }
+        newRow[newName] = newValue;
+    }
+    return newRow;
 }
